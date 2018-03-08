d2c4a1f414d634b517de6b6c0930c0ea8d31f660
crab <imrcrab@163.com>
2018-03-08 23:23:17 +0800

    List and Map learning

--- designOfModel/src/com/wpy/practise/adapter/AdapterMain.java
+++ designOfModel/src/com/wpy/practise/adapter/AdapterMain.java
@@ -1,9 +1,15 @@
 package com.wpy.practise.adapter;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class AdapterMain {
 	public static void main(String[] args) {
 		Target target = new Adapter();
 		target.Request();
+	
+		
+		
 	}
 	
 }
