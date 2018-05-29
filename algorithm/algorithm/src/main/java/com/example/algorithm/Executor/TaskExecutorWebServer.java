9bbe884b7ac810830a1801a0b54bb68a2b3baa73
imrcrab <imrcrab@163.com>
2018-05-29 23:39:05 +0800

    add somt comment

--- algorithm/algorithm/src/main/java/com/example/algorithm/Executor/TaskExecutorWebServer.java
+++ algorithm/algorithm/src/main/java/com/example/algorithm/Executor/TaskExecutorWebServer.java
@@ -8,12 +8,13 @@ import java.util.concurrent.Executors;
 
 public class TaskExecutorWebServer {
     private static final int NTHREADS = 100;
+    //初始化线程池大小
     private static final Executor exec = Executors.newFixedThreadPool(NTHREADS);
 
     /**
      * 基于线程池的web服务器.
      */
-    public static void ThreadMain() {
+    public static void threadMain() {
         try {
             ServerSocket serverSocket = new ServerSocket(80);
             while (true) {
@@ -28,5 +29,9 @@ public class TaskExecutorWebServer {
 
     private static void handelRequest(Socket socket) {
         System.out.println(socket.getPort() + "---name");
+    }
+
+    public static void main(String[] args) {
+        threadMain();
     }
 }
