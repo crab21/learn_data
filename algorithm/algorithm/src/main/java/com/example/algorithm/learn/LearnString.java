46adcce94a2ecebc20516b7d793289d290543a1f
imrcrab <imrcrab@163.com>
2018-05-31 23:59:45 +0800

    内部类作用域

--- algorithm/algorithm/src/main/java/com/example/algorithm/learn/LearnString.java
+++ algorithm/algorithm/src/main/java/com/example/algorithm/learn/LearnString.java
@@ -2,10 +2,12 @@ package com.example.algorithm.learn;
 
 import com.google.common.base.Joiner;
 import com.google.common.base.MoreObjects;
+import com.google.common.base.Strings;
 import com.google.common.collect.ComparisonChain;
 import com.google.common.collect.Lists;
 import com.google.common.collect.Maps;
 import com.google.common.collect.Sets;
+import org.apache.commons.lang3.StringUtils;
 
 import javax.validation.constraints.NotNull;
 import java.util.HashSet;
@@ -20,11 +22,10 @@ public class LearnString {
 
     /**
      * Guava包的使用.
-     *
-     * @param args
      */
-    public static void main(String[] args) {
+    public static void CollectTest() {
         HashSet<Object> objects = Sets.newHashSet();
+
         Maps.newHashMap();
         int result = ComparisonChain.start().compare(1, 1).compare(3, 3).compare(2, 2).result();
         System.out.println(result);
@@ -36,7 +37,27 @@ public class LearnString {
         String join = Joiner.on(".").join(list);
 
         System.out.println(join);
+    }
 
+    private String result = "";
 
+    public void stringTest() {
+        //s必须声明为final  下面的内部类才能使用
+        final String s = Strings.nullToEmpty("wang");
+        System.out.println(s);
+        class innerClass {
+            void pri() {
+                System.out.println(s);
+                ;
+                result = "";
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        String name = "   ";
+        if (StringUtils.isEmpty(name.trim())) {
+            System.out.println("ok");
+        }
     }
 }
