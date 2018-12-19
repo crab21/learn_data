b51d90c985fee41f17f5439a8a60306f2f3aeadb
imrcrab <imrcrab@163.com>
2018-12-19 23:42:04 +0800

    LRU算法学习(附带解释)-2

--- algorithm/src/main/java/com/example/algorithm/learnlist/LRU2.java
+++ algorithm/src/main/java/com/example/algorithm/learnlist/LRU2.java
@@ -19,16 +19,24 @@ public class LRU2<k, v> {
 
     private Map<k, Entry<k, v>> cacheMap;
 
+    /**
+     * 构造函数  设置LRU数量
+     * @param size
+     */
     public LRU2(int size) {
         this.MAX_CACHE_SIZE = size;
         cacheMap = new HashMap<>();
     }
 
     public v get(k key) {
+        //获取当前key中的值
         Entry<k, v> kvEntry = cacheMap.get(key);
+        //当前key值为null  则直接返回
         if(kvEntry == null) return null;
 
+        //移动当前获取的key对应的值 到链表头部
         movetoHead(kvEntry);
+        //返回值
         return kvEntry.value;
     }
 
@@ -64,30 +72,41 @@ public class LRU2<k, v> {
         }
     }
     private void movetoHead(Entry<k, v> entry) {
+        //当前为head节点 则无需移动
         if (entry == head) {
             return;
         }
 
+        //前一个节点不为空，则将当前节点的下一个元素  给当前节点的前一个->下一个
+        // 类似于跳过当前元素 把当前元素的下一个元素恰当处理
+        // ***处理当前节点的后节点关系
         if (entry.pre != null) {
             entry.pre.next = entry.next;
         }
 
+        //类似于前一个判断   ***处理当前节点的前节点关系
         if (entry.next != null) {
-            entry.pre.pre = entry.pre;
+            entry.next.pre = entry.pre;
         }
 
+        //判断当前元素是否为尾部节点
         if (entry == tail) {
+            //获取当前节点的前节点
             Entry<k, v> prev = entry.pre;
+            //前节点有值，则把前一个节点当做尾节点
+            //类似删除尾节点
             if (prev != null) {
                 tail.pre = null;
                 tail = prev;
                 tail.next = null;
             }
         }
+        //没有元素  双向变一
         if (head == null || tail == null) {
             head = tail = entry;
             return;
         }
+        //把当前节点放在head节点之前
         entry.next = head;
         head.pre = entry;
         entry.pre = null;
